98d9c238d8df18ba8667ca7582f786e214ba80df
Paul Panther <niklas.rothe@posteo.de>
2024-12-07 00:34:29 +0100

    Bug Handling Deck edit

--- app/decks/forms.py
+++ app/decks/forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms.fields.simple import StringField, SubmitField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, ValidationError
+import sqlalchemy as sa
+
+from app import db
+from app.models import Deck
 
 
 class DeckEditForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     decklist = StringField('Link', validators=[Optional()])
     submit = SubmitField('Deck ändern')
+
+    def validate_name(self, name):
+        name = db.session.scalar(sa.select(Deck).where(
+            Deck.Name == name.data))
+        if name is not None:
+            raise ValidationError('Es gibt schon ein Deck mit diesem Namen.')
