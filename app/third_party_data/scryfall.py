4c2718074c9710be64b57103329e64dd0032a6dc
Paul Panther <niklas.rothe@posteo.de>
2024-12-27 00:55:58 +0100

    Fixed Function Name

--- app/third_party_data/scryfall.py
+++ app/third_party_data/scryfall.py
@@ -1,16 +1,16 @@
 from flask import current_app, render_template
 from flask_login import login_required
 
+from app import tasks
 from app.auth import role_required
 from app.main import bp
-from webstats import app
 
 
 @bp.route('/load_card_data', methods=['GET'])
 @role_required('admin')
 @login_required
 def load_card_data():
-    current_app.task_queue.enqueue(app.tasks.load_card_data)
+    current_app.task_queue.enqueue(tasks.get_card_data)
 
     return render_template('index.html')
 
