dc6e096b071eda97008d054a81ce71a2b0f831ce
Paul Panther <niklas.rothe@posteo.de>
2025-07-07 18:52:23 +0200

    ???

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(dotenv_path=os.path.join(basedir, '.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
